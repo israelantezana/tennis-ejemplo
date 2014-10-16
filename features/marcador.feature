5e47ada08b5bfe042bb0d9c656935ec9ae4b7bba
Israel Antezana R <israelantezana@gmail.com>
2014-10-16 05:55:04 -0400

    Iniciar tablero triangulando

--- features/marcador.feature
+++ features/marcador.feature
@@ -3,3 +3,9 @@ Feature: Marcador
  		Given inicio partida con dos jugadores
 		Then deberia ver marcador "0" para Jugador1
 		And deberia ver marcador "0" para Jugador2
+		
+	Scenario: Jugador1 anota
+	 	Given inicio partida con dos jugadores
+		When jugador1 anota
+		Then deberia ver marcador "15" para Jugador1
+		And deberia ver marcador "0" para Jugador2
--- features/step_definitions/marcador_steps.rb
+++ features/step_definitions/marcador_steps.rb
@@ -5,11 +5,9 @@ Given(/^inicio partida con dos jugadores$/) do
   click_button("Jugar")
 end
 
-Then(/^deberia ver marcador "(.*?)" para "(.*?)"$/) do |puntaje, jugador|
-  last_response.body.should =~ /class="marcador#{jugador}">0/m  
-end
-
-
 Then(/^deberia ver marcador "(.*?)" para Jugador(\d+)$/) do |marcador, nroJugador|
-  last_response.body.should =~ /class="marcadorJugador#{nroJugador}">0/m
+  last_response.body.should =~ /class="marcadorJugador#{nroJugador}">#{marcador}/m
+  last_response.body.should have_selector('div', 
+                                :class => "marcadorJugador#{nroJugador}",
+                                :content => marcador)
 end
