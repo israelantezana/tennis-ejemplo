5a5442d08944fb354367a8569e3e7e23d95c8b6d
Israel Antezana R <israelantezana@gmail.com>
2014-11-06 08:14:44 -0400

    Se anaden mas reglas de marcador con rspec, y se refactoriza

--- lib/scorer.rb
+++ lib/scorer.rb
@@ -6,17 +6,37 @@ class Scorer
   
   def marcador_de_jugador(nro_jugador)
     if (nro_jugador == 1)
-      @puntaje1.to_s
+      if deuce?
+        "Deuce"
+      else
+        @puntaje1.to_s
+      end
     else 
-      @puntaje2.to_s
+      if deuce?
+        "Deuce"
+      else
+        @puntaje2.to_s
+      end
     end
   end
   
   def anota_jugador(nro_jugador)
     if (nro_jugador == 1)
-      @puntaje1 += 15
+      @puntaje1 = incrementar_marcador(@puntaje1)
     else
-      @puntaje2 += 15
+      @puntaje2 = incrementar_marcador(@puntaje2)
+    end
+  end
+  
+  def deuce?
+    @puntaje1 == 40 && @puntaje2 == 40
+  end
+  
+  def incrementar_marcador(puntaje_actual)
+    if (puntaje_actual == 30)
+      puntaje = 40
+    else
+      puntaje = puntaje_actual + 15
     end
   end
 end
--- spec/lib/scorer_spec.rb
+++ spec/lib/scorer_spec.rb
@@ -2,26 +2,52 @@ require 'spec_helper'
 require 'scorer'
 
 describe Scorer do
-  it "empieza con ambos marcadores en 0" do
-    scorer = Scorer.new
-    expect(scorer.marcador_de_jugador(1)).to eq("0")
-    expect(scorer.marcador_de_jugador(2)).to eq("0")
+  before (:each) do
+    @scorer = Scorer.new
+  end
+  it "empieza con ambos marcadores en 0" do  
+    expect(@scorer.marcador_de_jugador(1)).to eq("0")
+    expect(@scorer.marcador_de_jugador(2)).to eq("0")
   end
   
   it "jugador 1 anota" do
-    scorer = Scorer.new
-    scorer.anota_jugador(1)
-    expect(scorer.marcador_de_jugador(1)).to eq("15")
-    expect(scorer.marcador_de_jugador(2)).to eq("0")
+    @scorer.anota_jugador(1)
+    expect(@scorer.marcador_de_jugador(1)).to eq("15")
+    expect(@scorer.marcador_de_jugador(2)).to eq("0")
   end
   
   it "jugador 2 anota dos veces" do
-    scorer = Scorer.new
-    scorer.anota_jugador(1)
-    scorer.anota_jugador(2)
-    scorer.anota_jugador(2)
-    expect(scorer.marcador_de_jugador(1)).to eq("15")
-    expect(scorer.marcador_de_jugador(2)).to eq("30")
+    @scorer.anota_jugador(1)
+    @scorer.anota_jugador(2)
+    @scorer.anota_jugador(2)
+    expect(@scorer.marcador_de_jugador(1)).to eq("15")
+    expect(@scorer.marcador_de_jugador(2)).to eq("30")
   end
+  
+  it "jugador1 anota 3 veces" do
+    @scorer.anota_jugador(1)
+    @scorer.anota_jugador(1)
+    @scorer.anota_jugador(1)
+    expect(@scorer.marcador_de_jugador(1)).to eq("40")
+  end
+  
+  it "jugador2 anota 3 veces" do
+    @scorer.anota_jugador(2)
+    @scorer.anota_jugador(2)
+    @scorer.anota_jugador(2)
+    expect(@scorer.marcador_de_jugador(2)).to eq("40")
+  end
+  
+  it "jugador1 y jugador 2 anotan 3 veces se muestra DEUCE" do
+    @scorer.anota_jugador(1)
+    @scorer.anota_jugador(1)
+    @scorer.anota_jugador(1)
+    @scorer.anota_jugador(2)
+    @scorer.anota_jugador(2)
+    @scorer.anota_jugador(2)
+    expect(@scorer.marcador_de_jugador(1)).to eq("Deuce")
+    expect(@scorer.marcador_de_jugador(2)).to eq("Deuce")
+  end
+  
   
 end
